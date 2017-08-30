59fcc3f0fe604eef8b232713b92185f7f98f6fa8
Melissa Rudick <mel.madrugada@gmail.com>
2017-08-30 04:53:39 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,7 +25,7 @@ def valid_move?(board, index)
   end
 end
 
-def move(board, index, value)
+def move(board, index, value = "X")
   board[index] = value
 end
 
@@ -34,7 +34,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    move(board, index, value)
+    move(board, index)
 display_board(board)
 else
   turn(board)
