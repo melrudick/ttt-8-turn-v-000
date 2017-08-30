4a7a4d5a27cd1e05aad98f2ee80cf438bac4f1d8
Melissa Rudick <mel.madrugada@gmail.com>
2017-08-30 02:40:07 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,7 +19,7 @@ elsif board[index] == "X" || board[index] == "O"
 end
 
 def valid_move?(board, index)
-  if board index.between?(0,8) && !position_taken?
+  if board[index] index.between?(0,8) && board[index] == !position_taken?
     true
   else
     false
