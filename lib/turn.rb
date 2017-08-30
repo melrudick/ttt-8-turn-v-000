a83ae2fd7dbd0ba5f2a61a492ac045157b53f009
Melissa Rudick <mel.madrugada@gmail.com>
2017-08-30 02:35:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
-  if board index.between?(0,8) && !position_taken? 
+  if board index.between?(0,8) && !position_taken?
     true
   else
     false
